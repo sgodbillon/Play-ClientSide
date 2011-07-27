0c588a53b8121a40c84009589f81c0e9dfb410b2
Stephane Godbillon <sgo@zenexity.com>
2011-07-27 16:10:49 +0200

    support categories for exporting routes (WIP)

--- src/play/modules/clientside/ClientSidePlugin.java
+++ src/play/modules/clientside/ClientSidePlugin.java
@@ -23,32 +23,49 @@ public class ClientSidePlugin extends PlayPlugin {
     @Override
     public void onApplicationStart() {
     	System.out.println("reloading clientsideplugin");
-    	ClientSidePlugin.paramsNamesByAction = makeParamsNamesByAction();
+    	ClientSidePlugin.actionsByName = makeActionsByName();
     	clearCache();
     }
     
-    private static Map<String, List<List<String>>> makeParamsNamesByAction() {
-    	HashMap<String, List<List<String>>> paramsNamesByAction = new HashMap<String, List<List<String>>>();
+    public static class Action {
+        public String name;
+        public List<String> params;
+        public String[] categories;
+    }
+    
+    public static Map<String, List<Action>> actionsByName = new HashMap<String, List<Action>>();
+    
+    private static Map<String, List<Action>> makeActionsByName() {
+    	Map<String, List<Action>> actionsByName = new HashMap<String, List<Action>>();
         for(ApplicationClass applicationClass: Play.classes.getAssignableClasses(Controller.class)) {
             try {
-                for (Method action: applicationClass.javaClass.getDeclaredMethods()) {
-                    if (Modifier.isPublic(action.getModifiers()) && Modifier.isStatic(action.getModifiers()) && action.getReturnType().equals(Void.class)) {
-                        String actionName = action.getDeclaringClass().getName().substring("controllers.".length()) + "." + action.getName();
-                        ArrayList<List<String>> paramsNames = new ArrayList<List<String>>();
-                        ArrayList<String> params = new ArrayList<String>();
-                        for (String param : Java.parameterNames(action)) {
-                            // TODO Handle skipped parameters
-                            params.add(param);
+                for (Method actionMethod: applicationClass.javaClass.getDeclaredMethods()) {
+                    if (Modifier.isPublic(actionMethod.getModifiers()) && Modifier.isStatic(actionMethod.getModifiers()) && actionMethod.getReturnType().equals(Void.class)) {
+                        String actionName = actionMethod.getDeclaringClass().getName().substring("controllers.".length()) + "." + actionMethod.getName();
+                        List<Action> actions = actionsByName.get(actionName);
+                        if(actions == null) {
+                        	actions = new ArrayList<Action>();
+                        	actionsByName.put(actionName, actions);
                         }
-                        paramsNames.add(params);
-                        paramsNamesByAction.put(actionName, paramsNames);
+                        Action action = new Action();
+                        if(actionMethod.isAnnotationPresent(ClientRoute.class)) {
+                        	action.categories = actionMethod.getAnnotation(ClientRoute.class).value();
+                        } else {
+                        	action.categories = new String[0];
+                        }
+                        action.params = new ArrayList<String>();
+                        for (String param : Java.parameterNames(actionMethod)) {
+                            // TODO Handle skipped parameters
+                        	action.params.add(param);
+                        }
+                        actions.add(action);
                     }
                 }
             } catch (Exception e) {
                 throw new UnexpectedException(e);
             }
         }
-        return paramsNamesByAction;
+        return actionsByName;
     }
     
     private static void clearCache() {
@@ -56,6 +73,4 @@ public class ClientSidePlugin extends PlayPlugin {
     		Cache.delete(key);
     	}
     }
-    
-    protected static Map<String, List<List<String>>> paramsNamesByAction = new HashMap<String, List<List<String>>>();
 }
--- src/play/modules/clientside/Routes.java
+++ src/play/modules/clientside/Routes.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import play.cache.Cache;
+import play.modules.clientside.ClientSidePlugin.Action;
 import play.mvc.Router;
 import play.mvc.Router.Route;
 
@@ -25,25 +26,47 @@ public class Routes {
 		String json = Cache.get(CACHE_KEY, String.class);
 		if(json == null) {
 			System.out.println("cache miss");
-			json = makeJSONFor(Router.routes);
+			json = makeJSONFor();
 			Cache.add(CACHE_KEY, json);
 		}else System.out.println("was in cache");
 		return json;
 	}
 	
-    public static String makeJSONFor(List<Route> routes) {
+    public static String makeJSONFor(String... categories) {
+    	List<Route> routes = Router.routes;
         ArrayList<ExtendedRoute> extendedRoutes = new ArrayList<ExtendedRoute>();
         for(Route route : routes) {
+        	List<Action> matchingActions = matchingActionsFor(route.action, categories);
             ExtendedRoute extendedRoute = new ExtendedRoute();
             extendedRoute.action = route.action;
             extendedRoute.method = route.method;
             extendedRoute.pattern = route.path;
-            extendedRoute.params = ClientSidePlugin.paramsNamesByAction.get(extendedRoute.action);
+            extendedRoute.params = new ArrayList<List<String>>();
+            for(Action action: matchingActions)
+        		extendedRoute.params.add(action.params);
             extendedRoutes.add(extendedRoute);
         }
         Type listType = new TypeToken<List<ExtendedRoute>>() { }.getType();
         String result = new Gson().toJson(extendedRoutes, listType);
         System.out.println("result is \n\t" + result);
         return result;
+    }
+    
+    private static List<Action> matchingActionsFor(String actionName, String... categories) {
+    	ArrayList<Action> result = new ArrayList<ClientSidePlugin.Action>();
+    	for(Action action: ClientSidePlugin.actionsByName.get(actionName)) {
+    		if(categories == null || categories.length == 0 || intersect(action.categories, categories))
+    			result.add(action);
+    	}
+    	return result;
+    }
+    
+    private static boolean intersect(String[] a1, String[] a2) {
+    	if(a1.length > 0 && a2.length > 0)
+    		for(int i = 0; i < a1.length; i++)
+    			for(int j = 0; j < a2.length; j++)
+    				if(a1[i].equals(a2[j]))
+    					return true;
+    	return false;
     }
 }
